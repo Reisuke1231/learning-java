6806679af65bb5c61c463ad5ff31a8a1e94a5ba9
Reisuke1231 <reisuke1231@gmail.com>
2022-04-01 16:09:11 +0900

    Salary calculator:in main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        double result = Math.pow(2, 5);
-        System.out.println(result);
-    }
+        System.out.println("Input the number of hours you works per week");
+        Scanner inputHours = new Scanner(System.in);
+        double hoursPerWeek = inputHours.nextDouble();
+
+        System.out.println("Input the amount of money you makes per hour");
+        Scanner inputMoney = new Scanner(System.in);
+        double moneyPerHour = inputMoney.nextDouble();
+
+        double weeksPerYear = 365 / 7;
+        double weeksDayOff = 120 / 7;
+        double weeksWorking = weeksPerYear - weeksDayOff;
+        double yearlySalary = moneyPerHour * hoursPerWeek * weeksWorking;
+
+        System.out.println("Annual salary is " + yearlySalary);}
 
 }
