99759074dd29be25b43b0d6266e30a51d034b3e7
Reisuke1231 <reisuke1231@gmail.com>
2022-04-01 11:50:35 +0900

    Change text

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! It's me Reisuke!");
+        System.out.println("Hello World! It's me Reisuke and I love SUSHI!");
     }
 }
