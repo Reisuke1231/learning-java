0d41c21bf4d44ad127c9ef6b5f337cbd32771101
Reisuke1231 <reisuke1231@gmail.com>
2022-04-02 13:00:04 +0900

    Instance and class variables in Java

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,15 @@
-import java.util.Scanner;
-
 public class Main {
+    public  static void main(String[] args) {
+        Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
+        Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
 
-    public static double salaryCalculator(double hoursPerWeek, double moneyPerHour) {
-        double weeksPerYear = 365 / 7;
-        double weeksDayOff = 120 / 7;
-        double weeksWorking = weeksPerYear - weeksDayOff;
+        double triangleAArea = triangleA.getArea();
+        System.out.println(triangleAArea);
 
-        return moneyPerHour * hoursPerWeek * weeksWorking;
+        double triangleBArea = triangleB.getArea();
+        System.out.println(triangleBArea);
+
+        System.out.println(Triangle.numOfSides);
     }
 
-    public static void main(String[] args) {
-        double hoursPerWeek;
-        double moneyPerHour;
-
-        do {
-            System.out.println("Input the number of hours you works per week");
-            Scanner inputHours = new Scanner(System.in);
-            hoursPerWeek = inputHours.nextDouble();
-        } while (hoursPerWeek <= 0);
-
-
-        do {
-            System.out.println("Input the amount of money you makes per hour");
-            Scanner inputMoney = new Scanner(System.in);
-            moneyPerHour = inputMoney.nextDouble();
-        } while (moneyPerHour <= 0);
-
-        double yearlySalary = salaryCalculator(hoursPerWeek, moneyPerHour);
-
-        System.out.println("Annual salary is " + yearlySalary);}
 }
